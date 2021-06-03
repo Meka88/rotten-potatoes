b56b1b792a7749d32b8132fd6e03a069f1e90f69
meka <mekabolzhirova@gmail.com>
2021-06-02 20:18:57 -0400

    Routes

---
+++ controllers/reviews.js
@@ -0,0 +1,69 @@
+const Review = require('../models/review');
+
+module.exports = function(app) {
+    // get home route -> sending message
+    app.get('/', (req, res) => {
+      Review.find()
+        .then(reviews => {
+          res.render('reviews-index', {reviews: reviews});
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+    app.get('/reviews/new', (req,res) => {
+        res.render('reviews-new', {title: "Post a Review"})
+    })
+    
+    app.post('/reviews', (req,res) => {
+        Review.create(req.body)
+            .then((review) => {
+                console.log(review);
+                res.redirect(`/reviews/${review._id}`)
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+        console.log(req.body.title);    
+    })
+    
+    app.get('/reviews/:id', (req, res) => {
+        Review.findById(req.params.id)
+            .then((review) => {
+                res.render('reviews-show', { review: review })
+            }).catch((err) => {
+                console.log(err.message);
+            })
+    })
+    
+    
+    // EDIT
+    app.get('/reviews/:id/edit', (req, res) => {
+        Review.findById(req.params.id, function(err, review) {
+          res.render('reviews-edit', {review: review, title: "Edit Review" });
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    })
+    
+    // UPDATE
+    app.put('/reviews/:id', (req, res) => {
+        Review.findByIdAndUpdate(req.params.id, req.body)
+          .then(review => {
+            res.redirect(`/reviews/${review._id}`)
+          })
+          .catch(err => {
+            console.log(err.message)
+          })
+      })
+    
+      // DELETE
+    app.delete('/reviews/:id', function (req, res) {
+        console.log("DELETE review")
+        Review.findByIdAndRemove(req.params.id).then((review) => {
+          res.redirect('/');
+        }).catch((err) => {
+          console.log(err.message);
+        })
+      })
+  }
