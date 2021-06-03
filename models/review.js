81cfe61a8f580a7c816695ea822b76728bc87243
meka <mekabolzhirova@gmail.com>
2021-06-02 20:20:54 -0400

    models

---
+++ models/review.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var reviewSchema = new Schema({
+    title: String,
+    movieTitle: String,
+    description: String
+});
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
