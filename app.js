c4d34f980d835ef96192bd60940ef79b5e0ca6dd
Meka88 <81331826+Meka88@users.noreply.github.com>
2021-06-03 19:52:25 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// require libraries
+const express = require('express');
+const Handlebars = require('handlebars');
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const app = express();
+const methodOverride = require('method-override')
+
+// middleware
+var exphbs = require('express-handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }), 
+methodOverride('_method'));
+app.use(express.static('public'));
+
+// set the templating engine -> handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main', 
+handlebars: allowInsecurePrototypeAccess(Handlebars)}))
+
+app.set('view engine', 'handlebars');
+
+// connect our server with MongoDB client
+mongoose.connect('mongodb+srv://meka_1904:987654321@cluster0.mvlw2.mongodb.net/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const reviews = require('./controllers/reviews')(app);
+
+
+// define app route
+app.listen(3000, () =>{
+    console.log(`App listening on port 3000!`);
+})
+
+
