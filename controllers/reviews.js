a0d0b8b7c2df3f9d6d15e34f421e5810de925db1
Meka88 <81331826+Meka88@users.noreply.github.com>
2021-06-08 16:59:45 -0400

    update reviews.js

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,6 +2,7 @@ const Review = require('../models/review');
 const Comment = require('../models/comment');
 
 module.exports = function (app) {
+
     // get home route, all reviews
     app.get('/', (req, res) => {
         Review.find().lean()
@@ -12,21 +13,24 @@ module.exports = function (app) {
                 console.log(err);
             })
     })
+
     // NEW REVIEW FORM (TEMPLATE)
     app.get('/reviews/new', (req, res) => {
         res.render('reviews-new', {title: "Post a Review"})
     })
+
     // CREATING A REVIEW
     app.post('/reviews', (req, res) => {
         Review.create(req.body)
             .then((review) => {
-                console.log(review);
+                console.log(review, res.status);
                 res.redirect(`/reviews/${review._id}`)
             })
             .catch((err) => {
                 console.log(err.message);
             })
     })
+
     // GETTING SINGLE REVIEW
     app.get('/reviews/:id', (req, res) => {
         Review.findById(req.params.id)
@@ -40,6 +44,7 @@ module.exports = function (app) {
                 console.log(err.message);
             });
     })
+
     // GETTING EDIT FORM
     app.get('/reviews/:id/edit', (req, res) => {
         Review.findById(req.params.id, function (err, review) {
@@ -48,6 +53,7 @@ module.exports = function (app) {
             console.log(err.message);
         });
     })
+
     // LOGIC FOR UPDATING A REVIEW
     app.put('/reviews/:id', (req, res) => {
         Review.findByIdAndUpdate(req.params.id, req.body)
@@ -57,6 +63,7 @@ module.exports = function (app) {
                 console.log(err.message);
             })
     })
+
     // LOGIC FOR DELETING A REVIEW
     app.delete('/reviews/:id', function (req, res) {
         console.log('Delete review');
@@ -67,4 +74,5 @@ module.exports = function (app) {
             console.log(err.message);
         })
     })
+
 }
